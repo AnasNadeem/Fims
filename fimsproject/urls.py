4b62e0d2d578fc46d748144363b21859aa236ace
Anas Nadeem <anas5678go@gmail.com>
2023-09-06 23:22:24 +0530

    Changed admin name

--- fimsproject/urls.py
+++ fimsproject/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('fimsadmin/', admin.site.urls),
     path("api/", include("fimsapp.urls")),
 ]
 
